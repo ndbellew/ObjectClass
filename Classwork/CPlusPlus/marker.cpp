9504f2aafb4753c9b7311f0580bba6df8bc1351f
ndbellew <ndbellew@mavs.coloradomesa.edu>
2019-02-12 13:23:46 -0700

    updated main cpp file

--- Classwork/CPlusPlus/marker.cpp
+++ Classwork/CPlusPlus/marker.cpp
@@ -1,2 +1,31 @@
 
 #include "Marker.h"
+
+namespace marker {
+    Marker::Marker(Color _color) {
+      color = _color;
+      capped = true;
+    }
+   Marker::Color getColor() {
+    return this->color;
+  }
+
+  Marker::bool isCapped(){
+    return this->capped;
+  }
+
+  Marker::void setColor(Color _color){
+    this->color = _color;
+  }
+
+  Marker::void setCapped(_capped){
+    this->capped=_capped;
+  }
+  Marker::void draw(){
+    if(!isCapped()){
+      cout<<"drawing in  "<<getColor()<<endl;
+    }else{
+      throw BAD_MARKER_CAPPED_DRAW
+    }
+  }
+}
