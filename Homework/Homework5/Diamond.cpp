5b490dfcbb8ae34bb73a98bb2b3de657a1efab8d
ndbellew <ndbellew@mavs.coloradomesa.edu>
2019-04-25 13:47:54 -0600

    I think I have it now?

--- Homework/Homework5/Diamond.cpp
+++ Homework/Homework5/Diamond.cpp
@@ -4,44 +4,23 @@
 #include "Drink"
 using namespace std;
 
-
-Drink::Drink(string n, int s){
-  name = n;
-  size = s;
-}
-void Drink::setname(string n){
-  name = n;
-}
-string Drink::getname(){
-  return name;
-}
-void Drink::setsize(int s){
-  size = s;
-}
-int Drink::getsize(){
-  return size;
-}
-
-class ColdDrink: virtual public Drink {
-protected:
-  float temperature;
-public:
-  ColdDrink(float temp);
-  void setTemp(float t);
-  float getTemp();
-};
-ColdDrink::ColdDrink(float t){
-  temperature = t;
-}
-void ColdDrink::setTemp(float t){
-  
-}
-
-class Rectangle: virtual public Polygon {
-protected:
-  double area;
+class ChessPiece{
+private: string name;
+private: int move;
+private: bool alive;
 };
 
-class Cuboid: public Square, public Rectangle{
-  double area, volume;
-}
+
+class Rook : extends ChessPiece{
+private: bool MoveVert;
+private: bool MoveHoriz;
+};
+
+class Bishop : extends ChessPiece{
+private: bool MovesDiag;
+};
+
+
+class Queen : extends Bishop, Rook{
+
+};
