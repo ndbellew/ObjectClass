b8f5fc8041766ac71aac525c20bfdc9dd245aa6a
ndbellew <ndbellew@mavs.coloradomesa.edu>
2019-04-26 14:20:53 -0600

    asfd

--- Homework/Homework5/Diamond.cpp
+++ Homework/Homework5/Diamond.cpp
@@ -8,16 +8,29 @@ class ChessPiece{
 private: string name;
 private: int move;
 private: bool alive;
+private: string color;
+public:
+  ChessPiece(string n, int m, bool a, string c);
+  void setname(string n);
+  void setAlive(bool a);
+  void setColor(string c);
+  string getname();
+  string getColor();
+  bool Isalive();
 };
 
 
 class Rook : extends ChessPiece{
-private: bool MoveVert;
-private: bool MoveHoriz;
+private: int move;
+public:
+  bool isMoveVert();
+  bool isMoveHoriz();
 };
 
 class Bishop : extends ChessPiece{
-private: bool MovesDiag;
+private: int move;
+public:
+    
 };
 
 
