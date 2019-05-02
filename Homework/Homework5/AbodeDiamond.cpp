c2485bdf45efab01143ec71a3eac36b992563903
ndbellew <ndbellew@gmail.com>
2019-05-02 09:55:20 -0600

    adding this to repo

---
+++ Homework/Homework5/AbodeDiamond.cpp
@@ -0,0 +1,11 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include "Abode.h"
+using namespace Abode;
+
+Abodes::Abodes(string n, int s, int r){
+  name = n;
+  size = s;
+  rooms = r;
+}
