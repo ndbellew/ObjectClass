05a124affea2291b8540c9d1fbaf65fbbf02cd69
ndbellew <ndbellew@gmail.com>
2019-05-07 12:29:22 -0600

    cleaning up the file, these were not needed for any assignment.

--- Homework/operatorTest.js
+++
@@ -1,9 +0,0 @@
-
-Phone1 = new Operator("0001", 1);
-Phone2 = new Operator("0002", 2);
-
-Phone1.makeCall(Phone1.Location, Phone2.Location);
-Phone1.StartCall();
-
-Phone1.PhoneActive(False);
-Phone1.EndCall();
