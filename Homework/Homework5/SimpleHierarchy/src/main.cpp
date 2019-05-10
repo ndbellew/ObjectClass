08402c6de201494031ecbb686c0aa202965ebf5c
ndbellew <ndbellew@gmail.com>
2019-05-10 00:51:38 -0600

    created tests, may need more.

--- Homework/Homework5/SimpleHierarchy/src/main.cpp
+++ Homework/Homework5/SimpleHierarchy/src/main.cpp
@@ -2,14 +2,32 @@
 #include <iomanip>
 #include <memory>
 #include <string>
+#include <assert.h>
 #include "../include/Bike.h"
 #include "../include/Carriage.h"
 
 using namespace vehicle;
 
+typedef std::shared_ptr < vehicle::Vehicle > V;
+typedef std::shared_ptr < vehicle::Bike > B;
+typedef std::shared_ptr < vehicle::Carriage > C;
 int main(){
-  Bike* bike = new Bike("Petar", BLACK, true, false);
-  Carriage *carriage = new Carriage("Todd", RED, 2, 300);
-  Vehicle *vehicle = new Vehicle(BLACK);
+  std::shared_ptr < Vehicle > BlueVehicle(new Vehicle(BLUE));
+  assert(BlueVehicle->getColor() == BLUE);
+  BlueVehicle->Drive();
+  std::cout<<"Shared pointer success\n";
+  C c(new Carriage("Diddle", Color::BRELLO, 3, 16));
+  assert(c->getHorsePower() == 3 );
+  std::cout<<"getter success\n";
+  c->setColor(Color::YELLOW);
+  assert(c->getColor() == Color::YELLOW);
+  std::cout<<"Setter and getter successful\n";
+  std::cout<<"Current Wheel Num "<<c->getWheelNum()<<std::endl;
+  c->setWheelNum(15);
+  assert(c->getWheelNum() == 15);
+  c->Drive();
+  B SweetBike(new Bike("Diddly", Color::TURQOISE, true, true));
+  //may have to rework the toggle in order to test :( this would me you have to add in BadVehicleState class. :(
+  SweetBike->Drive();
   return 0;
 }
